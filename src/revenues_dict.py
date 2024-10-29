5048cd125e592a17ce5df137ab443eed33bd8fc6
Jeferson Quadros <pianeiro@gmail.com>
2024-10-28 21:50:11 -0300

    feat: Resultados finais usados na LDO 2025.

---
+++ src/revenues_dict.py
@@ -0,0 +1,21 @@
+from typing import Dict, Union
+
+revenues_dict : Dict[str, Union[str, Dict[str, str]]] = {
+    '11000000000000000000' : {'cod': '11000000000000000000', 'desc': 'impostos', 'name': 'Impostos, Taxas e Contribuicoes de Melhoria'},
+    '11229000000000000000' : {'cod': '11225300000000000000', 'desc': 'limpeza_publica', 'name' : 'Taxa de limpeza pública'},
+    '11220111020000000000' : {'cod': '11225300000000000000', 'desc': 'limpeza_publica', 'name' : 'Taxa de limpeza pública'},
+    '11280291020000000000' : {'cod': '11225300000000000000', 'desc': 'limpeza_publica', 'name' : 'Taxa de limpeza pública'},
+    '11220101020000000000' : {'cod': '11225300000000000000', 'desc': 'limpeza_publica', 'name' : 'Taxa de limpeza pública'},
+    '11225300000000000000' : {'cod': '11225300000000000000', 'desc': 'limpeza_publica', 'name' : 'Taxa de limpeza pública'},
+    '12000000000000000000' : {'cod': '12000000000000000000', 'desc': 'contribuicoes', 'name' : 'Contribuicoes'},
+    '13000000000000000000' : {'cod': '13000000000000000000', 'desc': 'patrimonial', 'name' : 'Receita Patrimonial'},
+    '16000000000000000000' : {'cod': '16000000000000000000', 'desc': 'servicos', 'name' : 'Receita de Servicos'},
+    '17000000000000000000' : {'cod': '17000000000000000000', 'desc': 'transferencias', 'name' : 'Transferencias Correntes'},
+    '17210102000000000000' : {'cod': '17115100000000000000', 'desc': 'fpm', 'name' : 'FPM'},
+    '17180120000000000000' : {'cod': '17115100000000000000', 'desc': 'fpm', 'name' : 'FPM'},
+    '17115100000000000000' : {'cod': '17115100000000000000', 'desc': 'fpm', 'name' : 'FPM'},
+    '17220101000000000000' : {'cod': '17215000000000000000', 'desc': 'icms', 'name' : 'ICMS'},
+    '17280110000000000000' : {'cod': '17215000000000000000', 'desc': 'icms', 'name' : 'ICMS'},
+    '17215000000000000000' : {'cod': '17215000000000000000', 'desc': 'icms', 'name' : 'ICMS'},
+    '19000000000000000000' : {'cod': '19000000000000000000', 'desc': 'outras', 'name' : 'Outras Receitas Correntes'}
+}
